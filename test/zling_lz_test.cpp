9ab382960a8fcc490ee0a8c021e89b7b26b41584
richox <richselian@gmail.com>
2013-11-27 16:35:48 +0800

    zling/test

--- test/zling_lz_test.cpp
+++ test/zling_lz_test.cpp
@@ -8,17 +8,19 @@ TEST(ZlingLZ, Main) {
     baidu::zling::lz::ZlingRolzEncoder encoder;
     baidu::zling::lz::ZlingRolzDecoder decoder;
 
-    char data[2][256] = {
+    char data[2][512] = {
             "0.1.2.3.4.5.6.7.8.9"
             "9.8.7.6.5.4.3.2.1.0"
-            "match:123456"
-            "match:123789"
+            ".match:123456"
+            ".match:123789"
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ)!@#$%^&*(__(*&^%$#@!)XYXWVUTSRQPONMLKJIHGFEDCBA"
             "abcdefghijklmnopqrstuvwxyz0123456789==9876543210xyxwvutsrqponmlkjihgfedcba"
+            "aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz"
+            "AABBCCDDEEFFGGHHIIJJKKLLMMNNOOPPQQRRSSTTUUVVWWXXYYZZ"
     };
     int test_len = strlen(data[0]) + 1;
 
-    uint16_t test_encoded[1024];
+    uint16_t test_encoded[512];
     int encpos = 0;
     int decpos = 0;
 
@@ -28,8 +30,9 @@ TEST(ZlingLZ, Main) {
         test_len,
         test_len,
         &encpos);
+
     EXPECT_EQ(test_len, encpos);
-    EXPECT_EQ(test_len, rlen + 6);  // "atch:123" encoded into 2 symbols
+    EXPECT_EQ(test_len, rlen + 7);  // "match:123" encoded into 2 symbols
 
     int olen = decoder.Decode(
         test_encoded,
